7489cf2fc231afa67b131f411fc5ef841d06cbc4
Kimberley Kelly <kimberley.b.kelly@gmail.com>
2022-03-02 15:49:01 +0000

    refactor water tracker

--- src/components/water-tracker.js
+++ src/components/water-tracker.js
@@ -1,9 +1,17 @@
-document.querySelectorAll('.bottle').forEach((bottle) => {
-  const bottleCount = document.querySelector('.bottle-count')
+const bottleNodes = document.querySelectorAll('.bottle')
 
+bottleNodes.forEach((bottle) => {
   // toggle the class 'empty' when the bottle is clicked
-  bottle.addEventListener('click', function () {
-    bottle.classList.toggle('empty')
+  bottle.addEventListener('click', () => {
+    const bottleCount = document.querySelector('.bottle-count')
+    const isPressed = bottle.classList.contains('empty')
+
+    if (!isPressed) bottle.classList.add('empty')
+    else bottle.classList.remove('empty')
+
+    // since this is a non-traditional toggle button, we should update the aria pressed
+    // attribute to notify the user that it has changed
+    bottle.setAttribute('aria-pressed', isPressed)
 
     // grab the empty bottles length and display it
     const emptyBottles = document.getElementsByClassName('empty').length
